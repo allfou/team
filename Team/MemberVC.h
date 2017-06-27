ba0af2799d8efee5d42670648aa7b6d842788c9f
Fouad <info@staglab.com>
2017-06-26 22:42:27 -0700

    Added Member View

---
+++ Team/MemberVC.h
@@ -0,0 +1,13 @@
+//
+//  MemberVC.h
+//  Team
+//
+//  Created by Fouad Allaoui on 6/26/17.
+//  Copyright © 2017 Fouad Allaoui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemberVC : UIViewController
+
+@end
